706ac046f896d887b80eba231085c0ffd09f638a
Justin Albert <Jnalbert879@gmail.com>
2022-02-08 12:06:09 -0800

    add change password screen

--- client/src/screens/main/ChangePasswordScreen.tsx
+++ client/src/screens/main/ChangePasswordScreen.tsx
@@ -1,11 +1,117 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { View } from 'react-native';
+import ScreenWrapperComp from '../../shared/ScreenWrapperComp';
+import styled from 'styled-components/native';
+import { useForm } from 'react-hook-form';
+import { HeaderTextWrapper, HeaderText, SubheaderText, InputWrapper, SubmitButtonWrapper } from '../auth/SignInScreen';
+import StyledTextInput from '../../components/Inputs/StyledTextInput';
+import CircleButton from '../../shared/CircleButton';
+import ActionCompletedSection from '../../shared/ActionCompletedSection';
 
-const ChangePasswordScreen: FC = () => {
-  return (
-    <View>
+
+export interface ChangePasswordFormProps {
+  oldPassword: string;
+  newPassword: string;
+  confirmPassword: string;
+}
+
+const ChangePasswordScreen: FC<any> = ({navigation}) => {
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+    setError,
+    clearErrors
+  } = useForm<ChangePasswordFormProps>();
+
+  const onSubmit = (data: ChangePasswordFormProps) => {
+    console.log(data)
+    setIsSubmitted(true);
+  };
+
+  const handledButtonPress = () => {
+    const {oldPassword, newPassword, confirmPassword} = getValues();
+
+    // call function to validate password here instead of hard code
+    if (oldPassword !== "Louis16") {
+      setError("oldPassword", { type: "manual", message: "Incorrect Password" })
       
-    </View>
+    } else if (newPassword !== confirmPassword) {
+
+      const errorConfig = {type: "manual", message: "Passwords do not match"}
+      setError("newPassword", errorConfig)
+      setError("confirmPassword", errorConfig)
+
+    } else {
+      handleSubmit(onSubmit)()
+    }
+  }
+  
+  useEffect(() => {
+    console.log("worked")
+    clearErrors("oldPassword")
+  }, [getValues("oldPassword")])
+
+  const goBackToSettings = () => {
+    navigation.navigate("Profile", {screen: "Profile"})
+  }
+
+
+  return (
+    <>
+      {isSubmitted && <ActionCompletedSection visible={isSubmitted} title="Password Changed" subheading={"Your password has successfully been changed"} buttonTitle="Return To Settings" buttonOnPress={goBackToSettings} />}
+    <ScreenWrapperComp scrollView>
+      <HeaderTextWrapper>
+        <HeaderText>
+          Change Your Password
+        </HeaderText>
+        <SubheaderText>
+          Forgot you password? No worries
+        </SubheaderText>
+      </HeaderTextWrapper>
+
+      <InputWrapper>
+        <StyledTextInput
+          hideText={true}
+          error={errors.oldPassword}
+          rules={{ required: "This field is required" }}
+          control={control}
+          placeHolderText="Old Password"
+          name="oldPassword"
+          icon="password"
+        />
+
+        <StyledTextInput
+          hideText={true}
+          error={errors.newPassword}
+          rules={{ required: "This field is required" }}
+          control={control}
+          placeHolderText="New Password"
+          name="newPassword"
+          icon="password"
+          />
+          
+          <StyledTextInput
+          hideText={true}
+          error={errors.confirmPassword}
+          rules={{ required: "This field is required" }}
+          control={control}
+          placeHolderText="Confirm Password"
+          name="confirmPassword"
+          icon="password"
+        />
+      </InputWrapper>
+
+      <SubmitButtonWrapper>
+        <CircleButton onPress={handleSubmit(onSubmit)} />
+      </SubmitButtonWrapper>
+
+    </ScreenWrapperComp>
+    </>
   )
 }
 
