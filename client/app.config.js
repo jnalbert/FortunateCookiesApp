3d6a94952cf3d6a851368d629291fb865a858b92
Justin Albert <Jnalbert879@gmail.com>
2022-03-21 14:29:47 -0700

    fix version for app json

--- client/app.config.js
+++ client/app.config.js
@@ -4,7 +4,7 @@ export default {
     expo: {
         name: "Fortunate Cookies",
         slug: "fortunatecookies",
-        version: "1.0.0",
+        version: "1.0.1",
         orientation: "portrait",
         icon: "./assets/SmallLogo.png",
         splash: {
