59f4ecd9433e268f76773073af552f59cf505361
Justin Albert <Jnalbert879@gmail.com>
2022-03-25 17:07:25 -0700

    changed home screen

--- client/src/screens/main/DashboardScreen.tsx
+++ client/src/screens/main/DashboardScreen.tsx
@@ -100,7 +100,7 @@ const DashBoardScreen: FC = () => {
 
       <SectionWrapper>
         <SectionHeaderWrapper>
-          <SectionHeader>Our Company</SectionHeader>
+          <SectionHeader>Our Business</SectionHeader>
         </SectionHeaderWrapper>
         <BodyText>
         Fortunate Cookies looks to reinvent the out-of-touch fortunate cookie designs by putting a new age spin on an old sweet treat. We provide our customers with fun and interesting experiences through both helpful fortunes and pieces of advice. Upon opening our delicious sweet treats, you will feel no other option but to crack open a smile.
